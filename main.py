efb6f33d03875144a9994581505cbe058b4855ac
stevenbeers <stevenbeers@mac.com>
2022-10-07 14:28:56 -0400

    working on google sheet more

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ week_events_url_start = 'http://sports.core.api.espn.com/v2/sports/football/leag
 tracker_sheet_url = 'https://docs.google.com/spreadsheets/d/1XLPDd43v4ASAIDB_IVI0TU6IQmdqiPuueII3Xp9rOFQ/gviz/tq?tqx=out:csv&sheet='
 tracker_sheet_team_info_tab_name = 'TeamInfo'
 tracker_sheet_weekly_picks_tab_name = 'WeeklyPicks'
+football_teams_dict = dict()
+picks_dict = dict()
 teams_dict = dict()
-weeks_dict = dict()
+weekly_results_dict = dict()
 today_date = datetime.today()
 
 
@@ -20,17 +22,23 @@ today_date = datetime.today()
 # Functions
 ###################################
 def get_sheets_info():
-	teams_df = pd.read_csv(tracker_sheet_url + tracker_sheet_team_info_tab_name)
-	teams_temp_dict = teams_df.set_index('TeamUID').T.to_dict('list')
+	# Get football teams and create dictionary
+	football_teams_df = pd.read_csv(tracker_sheet_url + tracker_sheet_team_info_tab_name)
+	teams_temp_dict = football_teams_df.set_index('TeamUID').T.to_dict('list')
 	for uid in teams_temp_dict:
-		teams_dict[uid] = teams_temp_dict[uid][0]
+		football_teams_dict[uid] = teams_temp_dict[uid][0]
 	st.write('Team Info...')
-	st.write(teams_dict)
+	st.write(football_teams_dict)
 
+	# Get player weekly picks info
 	picks_df = pd.read_csv(tracker_sheet_url + tracker_sheet_weekly_picks_tab_name)
+	# Create teams dictionary
+	team_names = picks_df['Team'].unique()
+	st.write('Team Names...')
+	st.write(team_names)
 	picks_dict = picks_df.set_index('Name').T.to_dict('list')
-	st.write('Picks Info...')
-	st.write(picks_dict)
+	# st.write('Picks Info...')
+	# st.write(picks_dict)
 
 def get_teams_info():
 	response = requests.get(team_list_url)
@@ -45,7 +53,7 @@ def get_teams_info():
 		if team_object['isActive'] == True and team_object['isAllStar'] == False:
 			team_uid = team_object['uid']
 			team_name = team_object['displayName']
-			teams_dict[team_uid] = team_name
+			football_teams_dict[team_uid] = team_name
 			# print(f'COUNT:{team_counter}, UID="{team_uid}", NAME="{team_name}"')
 			team_counter += 1
 		link_counter += 1
@@ -100,6 +108,6 @@ if go_button:
 	# Cycle through each week and find the winners from each game
 	# for week in range(1, curr_week+1):
 	#	weekly_results = get_week_games(week)
-	#	weeks_dict[week] = weekly_results
-	#print(weeks_dict)
+	#	weekly_results_dict[week] = weekly_results
+	#print(weekly_results_dict)
 	st.write('Gathered all info...')
