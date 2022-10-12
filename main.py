ee29943206f50f6421ec61228901d7dd13011da0
stevenbeers <stevenbeers@mac.com>
2022-10-11 21:00:48 -0400

    Working with ranking numbers

--- main.py
+++ main.py
@@ -227,6 +227,7 @@ def calculate_rank_numbers(in_df: pd.DataFrame) -> pd.DataFrame:
 	saved_long_streak = 0
 	saved_curr_win_streak = 0
 	for index, player_entry in return_df.iterrows():
+		st.write(f'iterating over {player_entry}')
 		row_number += 1
 		if (player_entry['Total Points'] == saved_points) & (player_entry['Longest Streak'] == saved_long_streak) & \
 				(player_entry['Curr Win Streak'] == saved_curr_win_streak):
