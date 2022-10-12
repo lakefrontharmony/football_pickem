9a617abdb23331d2972d2e43acee353d50e95637
stevenbeers <stevenbeers@mac.com>
2022-10-11 21:03:11 -0400

    Working with ranking numbers

--- main.py
+++ main.py
@@ -230,10 +230,10 @@ def calculate_rank_numbers(in_df: pd.DataFrame) -> pd.DataFrame:
 		row_number += 1
 		if (player_entry['Total Points'] == saved_points) & (player_entry['Longest Streak'] == saved_long_streak) & \
 				(player_entry['Curr Win Streak'] == saved_curr_win_streak):
-			st.write(f'matching entry for {player_entry["Name"]}')
+			st.write(f'matching entry for {player_entry["Player"]}')
 			player_entry['Rank'] = rank_number
 		else:
-			st.write(f'non-matching entry for {player_entry["Name"]}')
+			st.write(f'non-matching entry for {player_entry["Player"]}')
 			player_entry['Rank'] = row_number
 			rank_number = row_number
 			saved_points = player_entry['Total Points']
