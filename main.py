d74fcb13b0758cb40844e3fd3d1376f8e524ecdb
stevenbeers <stevenbeers@mac.com>
2022-10-17 21:30:00 -0400

    Making the week selectable

--- main.py
+++ main.py
@@ -273,9 +273,12 @@ def calculate_rank_numbers(in_df: pd.DataFrame) -> pd.DataFrame:
 ###################################
 st.title("Football Pick'em Tracker")
 
+curr_week = get_week_num()
+
 # Get all teams uid and name and store in to team dictionary
 load_form = st.form('Show Calculations')
 load_form.write('Click the button below to see picks and weekly results')
+load_form.selectbox('Pick a Week', options=range(1, curr_week), index=curr_week)
 go_button = load_form.form_submit_button(label='Get info')
 
 if go_button:
@@ -286,8 +289,8 @@ if go_button:
 		# Retrieve team info from API's (commented as this is less efficient than getting from Google Sheet)
 		# st.write('Getting team info...')
 		# get_teams_info()
-		st.write('Getting current week...')
-		curr_week = get_week_num()
+		# st.write('Getting current week...')
+		# curr_week = get_week_num()
 		# curr_week = 5
 		st.write('Getting weekly info...')
 		# Cycle through each week and find the winners from each game
