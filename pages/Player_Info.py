6f4eef272e58f771e64f712954cbdf9f35f70366
stevenbeers <stevenbeers@mac.com>
2022-10-19 21:32:33 -0400

    Started Team Info Page and adding more placeholders for what will be on Player and Team pages.

--- pages/Player_Info.py
+++ pages/Player_Info.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
-st.write('This is a new page where player info is going to reside')
+st.title('Player Information')
+st.write('Pick a player')
+st.header('Info')
+st.write('Table with name, team, total points, longest streak, current streak')
+st.header('This week')
+st.write('Table with previous picks and win/loss')
+st.write('Matchups for this week and who are the favorites')
---
+++ pages/Team_Info.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('Team Information')
+st.header('Weekly Games')
+st.write('Table for each week')
+st.write('Each row is one game. Date, Home Team, Away Team, Spread, Over/Under, Winner')
+
+st.header('Team Results')
+st.write('One Table for each team')
+st.write('Team Name, Overall Record')
+st.write('Each row is one week for that team. Week Num, W/L, Were they the Favorite, Home/Away')
