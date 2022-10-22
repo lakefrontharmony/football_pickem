977846c64c525be8bcc0a374b485e4b68a8e6f15
stevenbeers <stevenbeers@mac.com>
2022-10-21 20:39:19 -0400

    Removed variables from variables.py and put back in main.py

--- main.py
+++ main.py
@@ -7,82 +7,99 @@ import streamlit as st
 ###################################
 # Variables
 ###################################
-import variables as v
+# Scoreboard link shows the real-time status of this week's games.
+# scoreboard_url = 'https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard'
+team_list_url = 'https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/2022/teams'
+season_url = 'http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/2022?lang=en&region=us'
+week_events_url_start = 'http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/2022/types/2/weeks/'
+week_events_url_end = '/events?lang=en&region=us'
+tracker_sheet_url = 'https://docs.google.com/spreadsheets/d/1XLPDd43v4ASAIDB_IVI0TU6IQmdqiPuueII3Xp9rOFQ/gviz/tq?tqx=out:csv&sheet='
+tracker_sheet_team_info_tab_name = 'TeamInfo'
+tracker_sheet_weekly_picks_tab_name = 'WeeklyPicks'
+football_teams_dict = dict()
+picks_dict = dict()
+teams_dict = dict()
 weekly_results_dict = dict()
+picks_df = pd.DataFrame()
+scores_df = pd.DataFrame()
+display_df = pd.DataFrame()
+ranking_df = pd.DataFrame()
+has_curr_week_game_happened_for_player = pd.DataFrame()
+today_date = datetime.today()
 
 ###################################
 # Functions
 ###################################
 # Find teams in the picks_df which match the input string and add to the teams dictionary
 def find_matching_users(in_row: str):
-	team_mask = v.picks_df['Team'] == in_row
-	team_names = v.picks_df.loc[team_mask]
+	team_mask = picks_df['Team'] == in_row
+	team_names = picks_df.loc[team_mask]
 	name_array = team_names['Name'].to_numpy()
-	v.teams_dict[in_row] = name_array
+	teams_dict[in_row] = name_array
 
 
 # Get team info and player pick info from Google Sheets.
 def get_sheets_info(in_tab_name: str) -> dict:
 	# Get football teams and create dictionary
-	football_teams_df = pd.read_csv(v.tracker_sheet_url + in_tab_name)
+	football_teams_df = pd.read_csv(tracker_sheet_url + in_tab_name)
 	teams_temp_dict = football_teams_df.set_index('TeamUID').T.to_dict('list')
 	for uid in teams_temp_dict:
-		v.football_teams_dict[uid] = teams_temp_dict[uid][0]
+		football_teams_dict[uid] = teams_temp_dict[uid][0]
 
 	# Get player weekly picks info
-	picks_sheet = pd.read_csv(v.tracker_sheet_url + v.tracker_sheet_weekly_picks_tab_name)
+	picks_sheet = pd.read_csv(tracker_sheet_url + tracker_sheet_weekly_picks_tab_name)
 	# Had a random problem with the picks_sheet, so I brute-force converted it to picks_df
-	v.picks_df['Name'] = picks_sheet['Name']
-	v.picks_df['Team'] = picks_sheet['Team']
-	v.picks_df['Week 1'] = picks_sheet['Week 1']
-	v.picks_df['Week 2'] = picks_sheet['Week 2']
-	v.picks_df['Week 3'] = picks_sheet['Week 3']
-	v.picks_df['Week 4'] = picks_sheet['Week 4']
-	v.picks_df['Week 5'] = picks_sheet['Week 5']
-	v.picks_df['Week 6'] = picks_sheet['Week 6']
-	v.picks_df['Week 7'] = picks_sheet['Week 7']
-	v.picks_df['Week 8'] = picks_sheet['Week 8']
-	v.picks_df['Week 9'] = picks_sheet['Week 9']
-	v.picks_df['Week 10'] = picks_sheet['Week 10']
-	v.picks_df['Week 11'] = picks_sheet['Week 11']
-	v.picks_df['Week 12'] = picks_sheet['Week 12']
-	v.picks_df['Week 13'] = picks_sheet['Week 13']
-	v.picks_df['Week 14'] = picks_sheet['Week 14']
-	v.picks_df['Week 15'] = picks_sheet['Week 15']
-	v.picks_df['Week 16'] = picks_sheet['Week 16']
-	v.picks_df['Week 17'] = picks_sheet['Week 17']
-	v.picks_df['Week 18'] = picks_sheet['Week 18']
+	picks_df['Name'] = picks_sheet['Name']
+	picks_df['Team'] = picks_sheet['Team']
+	picks_df['Week 1'] = picks_sheet['Week 1']
+	picks_df['Week 2'] = picks_sheet['Week 2']
+	picks_df['Week 3'] = picks_sheet['Week 3']
+	picks_df['Week 4'] = picks_sheet['Week 4']
+	picks_df['Week 5'] = picks_sheet['Week 5']
+	picks_df['Week 6'] = picks_sheet['Week 6']
+	picks_df['Week 7'] = picks_sheet['Week 7']
+	picks_df['Week 8'] = picks_sheet['Week 8']
+	picks_df['Week 9'] = picks_sheet['Week 9']
+	picks_df['Week 10'] = picks_sheet['Week 10']
+	picks_df['Week 11'] = picks_sheet['Week 11']
+	picks_df['Week 12'] = picks_sheet['Week 12']
+	picks_df['Week 13'] = picks_sheet['Week 13']
+	picks_df['Week 14'] = picks_sheet['Week 14']
+	picks_df['Week 15'] = picks_sheet['Week 15']
+	picks_df['Week 16'] = picks_sheet['Week 16']
+	picks_df['Week 17'] = picks_sheet['Week 17']
+	picks_df['Week 18'] = picks_sheet['Week 18']
 
 	# Create teams dictionary
-	team_names = v.picks_df['Team'].unique()
+	team_names = picks_df['Team'].unique()
 	vector_function = np.vectorize(find_matching_users)
 	vector_function(team_names)
-	return_df = v.picks_df.drop(columns=['Team'])
+	return_df = picks_df.drop(columns=['Team'])
 	return return_df.set_index('Name').T.to_dict('list')
 
 
 # Retrieve Football team info from API
 def get_teams_info():
-	response = requests.get(v.team_list_url)
+	response = requests.get(team_list_url)
 	all_teams = response.json()
 	teams_count = all_teams['count']
 	team_counter = 1
 	link_counter = 1
 	while team_counter <= teams_count:
-		team_link = v.team_list_url + '/' + str(link_counter)
+		team_link = team_list_url + '/' + str(link_counter)
 		team_response = requests.get(team_link)
 		team_object = team_response.json()
 		if team_object['isActive'] is True and team_object['isAllStar'] is False:
 			team_uid = team_object['uid']
 			team_name = team_object['displayName']
-			v.football_teams_dict[team_uid] = team_name
+			football_teams_dict[team_uid] = team_name
 			team_counter += 1
 		link_counter += 1
 
 
 # Lookup the current week number through API
 def get_week_num() -> int:
-	response = requests.get(v.season_url)
+	response = requests.get(season_url)
 	season = response.json()
 	week_num = season['type']['week']['number']
 	return week_num
@@ -92,7 +109,7 @@ def get_week_num() -> int:
 def get_week_games(in_week_num: int) -> dict:
 	results_dict = dict()
 	week_num = str(in_week_num)
-	week_events_url = v.week_events_url_start + week_num + v.week_events_url_end
+	week_events_url = week_events_url_start + week_num + week_events_url_end
 	week_response = requests.get(week_events_url)
 	all_weeks = week_response.json()
 	for week in all_weeks['items']:
@@ -101,7 +118,7 @@ def get_week_games(in_week_num: int) -> dict:
 		if event_response.status_code == 200:
 			event_object = event_response.json()['competitions'][0]
 			event_datetime = datetime.strptime(event_object['date'], '%Y-%m-%dT%H:%MZ')
-			if event_datetime < v.today_date:
+			if event_datetime < today_date:
 				if 'winner' in event_object['competitors'][0].keys():
 					results_dict[event_object['competitors'][0]['uid']] = event_object['competitors'][0]['winner']
 					results_dict[event_object['competitors'][1]['uid']] = event_object['competitors'][1]['winner']
@@ -115,32 +132,32 @@ def get_week_games(in_week_num: int) -> dict:
 
 def calculate_player_results() -> pd.DataFrame:
 	return_df = pd.DataFrame()
-	v.has_curr_week_game_happened_for_player['Dummy'] = range(0, 1)
+	has_curr_week_game_happened_for_player['Dummy'] = range(0, 1)
 	return_df['Week'] = range(1, 19)
-	v.display_df['Week'] = range(1, 19)
-	for player in v.picks_dict:
+	display_df['Week'] = range(1, 19)
+	for player in picks_dict:
 		return_df[player] = 0
-		v.display_df[player] = " "
-		v.has_curr_week_game_happened_for_player[player] = False
+		display_df[player] = " "
+		has_curr_week_game_happened_for_player[player] = False
 		for week_num in weekly_results_dict:
 			week_results = weekly_results_dict[week_num]
-			week_pick = v.picks_dict[player][week_num-1]
+			week_pick = picks_dict[player][week_num-1]
 			# Verify that the pick has been entered for this week
 			if type(week_pick) == str:
 				# Verify that the results of the game for that week exist
 				if week_pick in week_results.keys():
 					if week_num == st.session_state['week_num']:
-						v.has_curr_week_game_happened_for_player[player] = True
+						has_curr_week_game_happened_for_player[player] = True
 					if week_results[week_pick] is True:
 						return_df.at[week_num-1, player] = 1
-						v.display_df.at[week_num-1, player] = f'{v.football_teams_dict[week_pick]} - WIN'
+						display_df.at[week_num-1, player] = f'{football_teams_dict[week_pick]} - WIN'
 					else:
 						return_df.at[week_num-1, player] = 0
-						v.display_df[player] = v.display_df[player].astype(str)
-						v.display_df.at[week_num - 1, player] = f'{v.football_teams_dict[week_pick]} - LOSS'
+						display_df[player] = display_df[player].astype(str)
+						display_df.at[week_num - 1, player] = f'{football_teams_dict[week_pick]} - LOSS'
 				else:
-					v.display_df.at[week_num - 1, player] = f'{v.football_teams_dict[week_pick]}'
-	v.has_curr_week_game_happened_for_player.drop(columns=['Dummy'], inplace=True)
+					display_df.at[week_num - 1, player] = f'{football_teams_dict[week_pick]}'
+	has_curr_week_game_happened_for_player.drop(columns=['Dummy'], inplace=True)
 	return return_df
 
 
@@ -149,8 +166,8 @@ def calc_player_totals() -> pd.DataFrame:
 	return_df = pd.DataFrame()
 	# This is a dummy column to force the shape of the Dataframe (brute force)
 	return_df['Totals'] = range(0, 1)
-	for player in v.picks_dict:
-		return_df[player] = v.scores_df[player].sum()
+	for player in picks_dict:
+		return_df[player] = scores_df[player].sum()
 	# Drop the dummy column
 	return_df.drop(columns=['Totals'], inplace=True)
 	return return_df
@@ -161,10 +178,10 @@ def calc_team_totals() -> pd.DataFrame:
 	return_df = pd.DataFrame()
 	# This is a dummy column to force the shape of the Dataframe (brute force)
 	return_df['Dummy'] = range(0, 1)
-	for team in v.teams_dict:
+	for team in teams_dict:
 		team_total = 0
-		for team_player in v.teams_dict[team]:
-			team_total += v.scores_df[team_player].sum()
+		for team_player in teams_dict[team]:
+			team_total += scores_df[team_player].sum()
 		return_df.at[0, team] = team_total
 		return_df[team] = return_df[team].astype(int)
 	# Drop the dummy columns
@@ -200,15 +217,15 @@ def rank_players() -> pd.DataFrame:
 	return_rank_df = pd.DataFrame(columns=['Entry Order', 'Player', 'Rank',
 										   'Total Points', 'Longest Streak', 'Curr Win Streak'])
 	entry_order = 1
-	for player in v.picks_df['Name']:
-		player_df = calculate_streak_lengths(v.scores_df[player].iloc[0:int(st.session_state['week_num'])], player)
+	for player in picks_df['Name']:
+		player_df = calculate_streak_lengths(scores_df[player].iloc[0:int(st.session_state['week_num'])], player)
 		total_points = player_df[player].sum()
 		max_streak = 0
 		if len(player_df['streak_counter'].loc[player_df[player] == 1]) > 0:
 			max_streak = max(player_df['streak_counter'].loc[player_df[player] == 1])
 		curr_win_streak = 0
 
-		if v.has_curr_week_game_happened_for_player[player].values[0]:
+		if has_curr_week_game_happened_for_player[player].values[0]:
 			if player_df[player].iloc[-1] == 1:
 				curr_win_streak = player_df['streak_counter'].iloc[-1]
 		else:
@@ -279,7 +296,7 @@ if go_button:
 		st.write(f'Running with week: {st.session_state["week_num"]}')
 		st.header('Prepping Calcs...')
 		st.write('Retrieving Player Picks...')
-		v.picks_dict = get_sheets_info(v.tracker_sheet_team_info_tab_name)
+		picks_dict = get_sheets_info(tracker_sheet_team_info_tab_name)
 		# Retrieve team info from API's (commented as this is less efficient than getting from Google Sheet)
 		# st.write('Getting team info...')
 		# get_teams_info()
@@ -293,10 +310,10 @@ if go_button:
 
 		st.header('Starting Calculations...')
 		st.write('Calculating results from each game...')
-		v.scores_df = calculate_player_results()
+		scores_df = calculate_player_results()
 		st.write('Calculating player totals...')
 		player_totals = calc_player_totals()
-		v.ranking_df = rank_players()
+		ranking_df = rank_players()
 		st.write('Calculating team totals...')
 		teams_totals = calc_team_totals()
 		st.write('Calculations completed...')
@@ -306,8 +323,8 @@ if go_button:
 	st.subheader('Team Totals')
 	st.write(teams_totals)
 	st.subheader('Player Ranking')
-	st.write(v.ranking_df)
+	st.write(ranking_df)
 	st.subheader('Weekly Picks')
-	st.write(v.display_df.astype(str).T)
+	st.write(display_df.astype(str).T)
 	st.subheader('Weekly Points')
-	st.write(v.scores_df.T)
+	st.write(scores_df.T)
